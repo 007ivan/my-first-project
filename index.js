6d9c5cbc79679b31171a3998dffeb7624dd93b92
Admin <ivan_kruk@ukr.net>
2016-11-18 20:24:36 +0200

    add my first project with slider 181116

---
+++ index.js
@@ -0,0 +1,17 @@
+/*$(document).ready(function() {
+  $('.textT').hide();
+  $('.textT').click(function() {
+      var $textT = $(this).next('.textT');
+      if ($textT.is(':hidden')) {
+        $textT.slideDown();
+        $(this).addClass('close');
+      } else {
+        $textT.fadeOut();
+        $(this).removeClass('close');
+      }
+    }
+  ); // окончание click
+}); // окончание ready
+
+/*
+
